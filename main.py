109c87411f48dc32a05ab07cfc3e47a3c9266cde
Latheesh Poondla <latheeshpoondla1@gmail.com>
2026-09-14 00:07:02 +0530

    test without do_sample qwen

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def vlm_inference(text, image_path, processor, model, tokenizer, args):
                 padding=True,
                 return_tensors="pt",
             ).to(model.device)
-            generated_ids = model.generate(**inputs, max_new_tokens=128, do_sample=False)
+            generated_ids = model.generate(**inputs, max_new_tokens=128)
             generated_ids_trimmed = [
                 out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
             ]
